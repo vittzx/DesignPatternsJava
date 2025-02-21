5fabd108da55e38f2140f16b58db0915a8eaa9af
Vitor Cosso <vitorcosso@gmail.com>
2025-02-21 16:16:13 -0300

    feat: implementing inteface, adapter and player(like some API from other 'service')

--- AdapterPattern/src/main/java/org/example/common_adapter/MusicPlayerApp.java
+++ AdapterPattern/src/main/java/org/example/common_adapter/MusicPlayerApp.java
@@ -1,4 +1,77 @@
 package org.example.common_adapter;
 
-public class MusicPlayerApp {
+public class MusicPlayerApp{
+
+    public static void main(String[] args) {
+        final String fileName = "Hotel California - Eagles";
+
+        AudioPlayer audioPlayer = new Mp3Player();
+        audioPlayer.play(fileName);
+
+        audioPlayer = new FlacAdapter(new FlacPlayer());
+        audioPlayer.play(fileName);
+
+        audioPlayer = new WavAdapter(new WavPlayer());
+        audioPlayer.play(fileName);
+    }
+}
+
+
+// interface
+
+interface AudioPlayer {
+    void play(String fileName);
+}
+
+// Adaptees -> Call the "API's"
+
+class Mp3Player implements AudioPlayer {
+    public void play(String fileName){
+        //execute all logic and return to the "main/principal" method
+        System.out.println("Executing mp3 file: " + fileName);
+    }
+}
+
+class WavPlayer {
+    public void playWav(String fileName){
+        System.out.println("Executing wav file: " + fileName);
+    }
+}
+
+class FlacPlayer {
+    public void playFlac(String fileName){
+        System.out.println("Executing flac file: " + fileName);
+    }
+}
+
+// Adaptaders
+
+class WavAdapter implements AudioPlayer{
+
+    private final WavPlayer wavPlayer;
+
+    public WavAdapter(WavPlayer wavPlayer){
+        this.wavPlayer = wavPlayer;
+    }
+
+    @Override
+    public void play(String fileName){
+        wavPlayer.playWav(fileName);
+    }
+}
+
+
+class FlacAdapter implements AudioPlayer{
+
+    private final FlacPlayer flacPlayer;
+
+    public FlacAdapter(FlacPlayer flacPlayer){
+        this.flacPlayer = flacPlayer;
+    }
+
+
+    @Override
+    public void play(String fileName) {
+        flacPlayer.playFlac(fileName);
+    }
 }
